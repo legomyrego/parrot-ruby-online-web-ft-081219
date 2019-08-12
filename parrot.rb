fd6ce153314a025240cf6b12f86ca03a4edcc5b5
Mark Rego <rego121@gmail.com>
2019-08-12 18:12:08 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string = "Squawk")
-  string = "hey how are you."
+  puts string = "hey how are you."
 
   return string
 
